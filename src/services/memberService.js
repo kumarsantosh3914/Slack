dce65743f3a415386f7265f9554cb56f85c060b0
Santosh Kumar <santoshkumar15novmth@gmail.com>
2025-09-26 00:12:25 +0530

    Small fix

--- src/services/memberService.js
+++ src/services/memberService.js
@@ -1,16 +1,20 @@
 import userRepository from "../repositories/userRepository.js";
 import workspaceRepository from "../repositories/workspaceRepository.js";
-import { UnauthorizedError } from "../utils/errors/app.error.js";
+import { NotFoundError, UnauthorizedError } from "../utils/errors/app.error.js";
 import { isUserMemberOfWorkspace } from "./workspaceService.js";
 
 export const isMemberPartOfWorkspaceService = async (workspaceId, memberId) => {
-    const workspace = await workspaceRepository.getById(workspaceId);
+  const workspace = await workspaceRepository.getById(workspaceId);
 
-    const isUserMember = isUserMemberOfWorkspace(workspace, memberId);
-    if (!isUserMember) {
-        throw new UnauthorizedError("User is not a member of the workspace");
-    }
+  if (!workspace) {
+    throw new NotFoundError("Workspace not found");
+  }
 
-    const user = await userRepository.getById(memberId);
-    return user;
-}
+  const isUserMember = isUserMemberOfWorkspace(workspace, memberId);
+  if (!isUserMember) {
+    throw new UnauthorizedError("User is not a member of the workspace");
+  }
+
+  const user = await userRepository.getById(memberId);
+  return user;
+};
